21d1b1564808d8207dfbf0bec311bc7daf9518e7
txrunishere <tsoni9286@gmail.com>
2025-12-29 22:54:53 +0530

    - improve code structure - add authMiddleware for authentication

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
 export default {
   JWT_SECRET: process.env.JWT_SECRET,
 };
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import config from "../config.js";
+
+export default async function authMiddleware(req, res, next) {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(403).json({
+      message: "Token not found",
+    });
+  }
+
+  const [type, tokenWithoutBearer] = token.split(" ");
+
+  if (!tokenWithoutBearer || type !== "Bearer") {
+    return res.status(403).json({
+      message: "Invalid Token",
+    });
+  }
+
+  try {
+    jwt.verify(tokenWithoutBearer, config.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "invalid or expired token",
+        });
+      }
+
+      req.user = decoded;
+      next();
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Authorization Error",
+    });
+  }
+}
