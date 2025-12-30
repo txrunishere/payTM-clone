62ab175a2ad01233e231083fb3ba5d3148102768
txrunishere <tsoni9286@gmail.com>
2025-12-30 20:48:24 +0530

    feat: integrate Tailwind CSS and set up routing
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Implemented routing in the App component with routes for Login, Register, and Dashboard pages.
    - Created new components for Dashboard, Login, and Register pages.
    - Updated main entry point to include BrowserRouter for routing support.
    - Imported Tailwind CSS in the global stylesheet.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router";
+import { Dashboard, Login, Register } from "./components/pages";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/pages/index.js
@@ -0,0 +1,5 @@
+import { Dashboard } from "./dashboard";
+import { Login } from "./login";
+import { Register } from "./register";
+
+export { Dashboard, Login, Register };
